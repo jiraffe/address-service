068a5c2131f271baff84b26dc002783a159423a9
dzmitryDubrovin <dzmitry_dubrovin@dancosoft.com>
2014-01-28 16:39:28 +0300

    adding message constants

--- server-side/src/main/java/com/danco/addresswrap/web/AddressRequestController.java
+++ server-side/src/main/java/com/danco/addresswrap/web/AddressRequestController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -19,6 +20,7 @@ import com.danco.addresswrap.domain.Address;
 import com.danco.addresswrap.exception.AddressNotFoundException;
 import com.danco.addresswrap.service.AddressService;
 import com.danco.addresswrap.service.RequestMessageConverter;
+import com.danco.addresswrap.service.SynonymService;
 
 @Controller
 public class AddressRequestController {
@@ -29,9 +31,26 @@ public class AddressRequestController {
 	@Autowired
 	private AddressService addressService;
 	
+	@Autowired
+	private SynonymService synonymService;
+	
+	//error messages
 	private static final String PARSING_ERROR_MSG = "Error in parsing";
 	private static final String ADDRESS_NOT_FOUND_MSG = "Address not found exception";
 	private static final String CHECKING_SERVICES_NOT_AVALIABLE = "One or more services for checking address are not avaliable";
+	private static final String PARAMS_NOT_FILLED = "Check address parameters";
+
+	//response object messages 
+	private static final String MESSAGE_KEY = "message";
+	private static final String SUCCESS_KEY = "success";
+	private static final String DATA_KEY = "data";
+
+	//mapping
+	private static final String CHECK_URL = "/check";
+	private static final String ADD_URL = "/add";
+
+	//JSON keys
+	private static final String SYNONYMS_KEY = "synonyms";
 	
 	
 	/**
@@ -40,11 +59,11 @@ public class AddressRequestController {
 	 * @return
 	 * @throws Exception
 	 */
-	@RequestMapping(value="/check", method = RequestMethod.GET)
+	@RequestMapping(value=CHECK_URL, method = RequestMethod.GET)
     public @ResponseBody Map<String, Object> getAddress(@RequestParam(value="city") String city, @RequestParam(value="street") String street, @RequestParam(value="building") String building) {
 		
 		if((city == null || city.length() == 0) || (street == null || street.length() == 0) || (building == null || building.length() == 0))	{
-			return mapError("Check address parameters");
+			return mapError(PARAMS_NOT_FILLED);
 		}
 		
 		Address address;
@@ -60,22 +79,36 @@ public class AddressRequestController {
 		}
     }
 	
-	@RequestMapping(value="/add", method = RequestMethod.POST)
+	@RequestMapping(value=ADD_URL, method = RequestMethod.POST)
 	public @ResponseBody Map<String, Object> addAddress(@RequestBody String data) {
 		
-		JSONObject parsedAddress;
+		JSONObject parsedObject;
 		try {
 			JSONParser parser = new JSONParser();
-			parsedAddress = (JSONObject)parser.parse(data);
+			parsedObject = (JSONObject)parser.parse(data);
 		} catch (ParseException e) {
 			return mapError(ADDRESS_NOT_FOUND_MSG);
 		}
 		
-		addressService.saveAddress(parsedAddress);
+		Address address = addressService.saveAddress(parsedObject);
 		
-		return mapSuccess(data);
+		//get synonyms
+		JSONArray synonyms = (JSONArray) parsedObject.get(SYNONYMS_KEY);
+		
+		if(synonyms.size() > 0)	{
+			synonymService.saveAddressSynonims(address, synonyms);
+		}
+		
+		return mapSuccess(address);
 	}
 	
+	@RequestMapping(value="/rel", method = RequestMethod.GET)
+	public @ResponseBody Map<String, Object> checkRel()	{
+		
+		Address address = addressService.getAddresBySynonimAndCity("гродно", "работа");	
+		
+		return mapSuccess(address);
+	}
 	
 	/**
 	 * Map error.
@@ -86,8 +119,8 @@ public class AddressRequestController {
 	public static Map<String,Object> mapError(String msg){
 
 		Map<String,Object> modelMap = new HashMap<String,Object>(2);
-		modelMap.put("message", msg);
-		modelMap.put("success", false);
+		modelMap.put(MESSAGE_KEY, msg);
+		modelMap.put(SUCCESS_KEY, false);
 
 		return modelMap;
 	} 
@@ -102,8 +135,8 @@ public class AddressRequestController {
 	public static Map<String, Object> mapSuccess(Object data) {
 		
 		Map<String,Object> modelMap = new HashMap<String,Object>();
-		modelMap.put("data", data);
-		modelMap.put("success", true);
+		modelMap.put(DATA_KEY, data);
+		modelMap.put(SUCCESS_KEY, true);
 		return modelMap;
 		
 	}
