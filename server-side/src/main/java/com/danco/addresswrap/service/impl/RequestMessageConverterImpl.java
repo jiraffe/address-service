149134c369ce4267b428479e70cbcde9dcd5fe9d
dzmitryDubrovin <dzmitry_dubrovin@dancosoft.com>
2014-01-29 12:28:35 +0300

    remove unused code

--- server-side/src/main/java/com/danco/addresswrap/service/impl/RequestMessageConverterImpl.java
+++ server-side/src/main/java/com/danco/addresswrap/service/impl/RequestMessageConverterImpl.java
@@ -46,66 +46,13 @@ public class RequestMessageConverterImpl implements RequestMessageConverter {
 	@Autowired
 	UnregistredAddressProcessor unregistredProcessor;
 	
-	private static final String SPACE = " ";
-	private static final String STREET_SUFIXES = "улица ул.";
-	private static Map<String, String> replacesMap = new HashMap<String, String>();
-	
-	private static final Set<String> CITIES = new HashSet<String>();
-	
-	private String targetCity;
-	private String[] addressNodes;
-	
 	/**
 	 * fill cities list
 	 * currently hardcode Grodno
 	 */
-	static {
-		CITIES.add("гродно");
-		
-		replacesMap.put("блк", "бульвар ленинского комсомла");
-	}
-	
-	public Address parseRequest(String request) throws Exception {
-		
-		for(String sufix: STREET_SUFIXES.split(SPACE))	{
-			request = request.replace(sufix, "");
-		}
-		
-		request = request.replace("  ", "");
-		
-		addressNodes = request.toLowerCase().trim().split(SPACE);
-		
-		for (int i = 0; i < addressNodes.length; i++) {
-			processNode(addressNodes[i], i);
-		}
-		
-		Address address = addressService.getAddress(targetCity, addressNodes[1], addressNodes[2]);
-		
-		if(address != null)	{
-			return address;
-		}
-		
-		RequestService.question = targetCity + " " + addressNodes[1] + " " + addressNodes[2];
-		
-		address = unregistredProcessor.processServiceChain();
-		address.setCity(targetCity);
-		addressService.saveAddress(address);
-		
-		return address;
-	}
-
-	private void processNode(String node, int index) {
-		if(CITIES.contains(node))	{
-			targetCity = node;
-		}
-		
-		if(replacesMap.containsKey(node))	{
-			addressNodes[index] = replacesMap.get(node);
-		}
-	}
 
 	@Override
-	public Address parseRequest(String city, String street, String building) throws AddressNotFoundException, IOException, ParseException {
+	public Address parseRequest(String city, String street, String building, String synonyms) throws AddressNotFoundException, IOException, ParseException {
 		
 		Address address = addressService.getAddress(city, street, building);
 		
@@ -114,7 +61,7 @@ public class RequestMessageConverterImpl implements RequestMessageConverter {
 		}
 		
 		//if address not found then try to find synonims
-		// TODO: process synonims
+		// TODO: process synonyms
 		
 		RequestService.question = city + "+" + street + "+" + building;
 		
